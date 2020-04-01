2be2625218f121b72eee170bb12d2833130b4472
israel <israel.bejarano.a@gmail.com>
2020-04-01 10:13:20 +0200

    mostrar colores del usuario

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -28,6 +28,9 @@ export class ChatComponent implements OnInit {
       this.client.subscribe('/chat/mensaje', (e) => {
         const mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
         mensaje.fecha = new Date(mensaje.fecha);
+        if (!this.mensaje.color && mensaje.tipo === 'NUEVO_USUARIO' && this.mensaje.username === mensaje.username) {
+          this.mensaje.color = mensaje.color;
+        }
         this.mensajes.push(mensaje);
         console.log(mensaje);
       });
