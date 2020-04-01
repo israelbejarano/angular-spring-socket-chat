d50c8eddf40f397c68569cc4ae21526177db30be
israel <israel.bejarano.a@gmail.com>
2020-04-01 11:21:44 +0200

    mostrar el historial de mms

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -14,9 +14,12 @@ export class ChatComponent implements OnInit {
   mensaje: Mensaje = new Mensaje();
   mensajes: Mensaje[] = [];
   escribiendo: string;
+  clienteId: string;
   conectado = false;
 
-  constructor() { }
+  constructor() {
+    this.clienteId = 'id-' + new Date().getTime() + '-' + Math.random().toString(36).substr(2);
+   }
 
   ngOnInit() {
     this.client = new Client();
@@ -41,12 +44,23 @@ export class ChatComponent implements OnInit {
           this.escribiendo = '';
         }, (5000));
       });
+      console.log(this.clienteId);
+      this.client.subscribe('/chat/historial/' + this.clienteId, (e) => {
+        const historial = JSON.parse(e.body) as Mensaje[];
+        this.mensajes = historial.map(m => {
+          m.fecha = new Date(m.fecha);
+          return m;
+        }).reverse();
+      });
+      this.client.publish({destination: '/app/historial', body: this.clienteId});
       this.mensaje.tipo = 'NUEVO_USUARIO';
       this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
     };
     this.client.onDisconnect = (frame) => {
       console.log('Desconectados: ' + !this.client.connected + ' : ' + frame);
       this.conectado = false;
+      this.mensaje = new Mensaje();
+      this.mensajes = [];
     };
   }
 
